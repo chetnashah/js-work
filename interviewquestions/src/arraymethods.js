5ba96177a0dc3dcdc6feff827e0a41c4824bf22a
chetnashah <chetshah80@gmail.com>
2022-05-27 11:36:33 +0530

    tgj

--- interviewquestions/src/arraymethods.js
+++ interviewquestions/src/arraymethods.js
@@ -1,10 +1,11 @@
 
 
 module.exports = {
-    ArrayFilter: function myfilter(predicate, thisArg) {
+    ArrayFilter: function myfilter(predicate, thisArg) {// note optional thisArg can be specified for callback
         var contextArr = this;// the array that this method is called on, different from thisArg which is for callback context
         var retArr = [];
         for(var i=0;i<contextArr.length;i++) {
+            // using call to give special thisArg to callback fn
             if(predicate.call(thisArg, contextArr[i],i, contextArr) === true) { // note all four params given to callback fn
                 retArr.push(contextArr[i]);
             }
