947d0712b61b1c96c718e1e9cce853d6dbfb9e64
chetnashah <chetshah80@gmail.com>
2022-08-17 11:46:28 +0530

    khj

--- interviewquestions/src/jsonparsestringify.js
+++ interviewquestions/src/jsonparsestringify.js
@@ -2,3 +2,96 @@
 // https://bigfrontend.dev/problem/implement-JSON-stringify
 // https://bigfrontend.dev/problem/implement-JSON-parse
 
+function JSONParse() {
+
+}
+
+
+/**
+ * Edge cases: passing undefined directly returns undefined value instead of "undefined" string
+ * But undefined in a array is converted to null e.g. JSON.stringify([1, undefined, 22]) -> "[1,null,22]"
+ * 
+ * undefined, Functions, and Symbols are not valid JSON values.
+ *  If encountered as value in an object, both key-value are ignored, 
+ * but if encountered in array, they are converted to null
+ * 
+ * Note how they can never be found in JSON
+ * that is transferred over the wire and used by other languages
+ * 
+ * The numbers Infinity and NaN, as well as the value null, are all considered null
+ * 
+ * numbers, booleans if encountered as values in object or array, they are not converted to strings 
+ * 
+ * @param {} v 
+ * @returns 
+ */
+let visited = new Set(); // to check for cyclic references Set is a good choice, since it stores objects as is
+function JSONStringify(v) {
+    visited = new Set();
+    return JSONStringifyHelper(v, false);
+}
+
+function JSONStringifyHelper(v, isInArrayOrObject) {
+    if(typeof v === "string" || v instanceof String) { // special check for String objects i.e. new String("hi")
+        return isInArrayOrObject ? `"${v}"` : v;
+    }
+    /**
+     *  * undefined, Functions, and Symbols are not valid JSON values.
+     *  If encountered as value in an object, both key-value are ignored, 
+    * but if encountered in array, they are converted to null
+     */
+    if(v === undefined || typeof v === "symbol" || typeof v === "function") {
+        return isInArrayOrObject ? "null" : undefined;
+    }
+
+    /**
+     *  * The numbers Infinity and NaN, as well as the value null, are all considered null
+     */
+    if(v === null || Number.isNaN(v) || v === Infinity) {
+        return "null";
+    }
+    if(typeof v !== "object" || v instanceof Boolean || v instanceof Number) {
+        return v.toString();
+    }
+
+    // typeof v is object: either array or object
+    let ans = '';
+    if(Array.isArray(v)) {
+        ans = ans + '[';
+        v.forEach((k, idx) => {
+            let s = JSONStringifyHelper(k, true);
+            ans = ans+s + (idx == v.length - 1 ? '' : ',');
+        })
+        ans = ans + ']';
+        return ans;
+    }
+
+    if(v.constructor === Date) {
+        return `"${v.toISOString()}"`;
+    }
+
+    if(visited.has(v)) {
+        throw new Error("cyclic object value");
+    }
+    if(typeof v === "object") {
+        ans = ans + '{';
+        visited.add(v);
+        const keys = Object.keys(v);
+        keys.forEach((k, idx) => {
+            let keyPart = `"${JSONStringifyHelper(k)}"`;
+            // if value part is one of undefined, symbol or function, skip that key/value
+            if(v[k] === undefined || typeof(v[k]) === "symbol" || typeof(v[k]) === "function") {
+                return;
+            }
+            let valuePart = JSONStringifyHelper(v[k], true);
+            ans = ans + (keyPart+ ":" +valuePart) + (idx === (keys.length - 1) ? '' : ',');
+        });
+        ans = ans + '}';
+        return ans;
+    }
+}
+
+module.exports = {
+    JSONParse,
+    JSONStringify
+};
--- interviewquestions/src/jsonparsestringify.test.js
+++ interviewquestions/src/jsonparsestringify.test.js
@@ -1,6 +1,84 @@
 
-describe("all json parse/stringify tests" ,() => {
-    it("bsic test" ,() => {
-        expect(4).toBe(4);
-    }) 
+const { JSONStringify, JSONParse } = require('./jsonparsestringify');
+
+describe("all json stringify tests" ,() => {
+    it("primitive test" ,() => {
+        expect(JSONStringify(1)).toEqual("1");
+        expect(JSONStringify("1")).toEqual("1");
+        expect(JSONStringify(null)).toEqual("null");
+        expect(JSONStringify(undefined)).toEqual(undefined);
+        expect(JSONStringify(NaN)).toEqual("null");
+    });
+
+    it('stringify complex data', () => {
+        expect(JSONStringify({})).toEqual("{}");                    // '{}'
+        expect(JSONStringify([1, 'false', false])).toEqual('[1,"false",false]');   // '[1,"false",false]'
+        expect(JSONStringify([NaN, null, Infinity])).toEqual('[null,null,null]'); // '[null,null,null]'
+        expect(JSONStringify({ x: 5 })).toEqual('{"x":5}');            // '{"x":5}'
+
+        expect(JSONStringify(JSON.stringify(function(){}))).toEqual(undefined);
+
+        expect(JSONStringify(new Date(Date.UTC(2006, 0, 2, 15, 4, 5)))).toEqual('"2006-01-02T15:04:05.000Z"');
+
+
+        expect(JSONStringify({ x: 5, y: 6 })).toEqual('{"x":5,"y":6}');
+        
+        // primitives with Constructors
+        expect(JSONStringify([new Number(3), new String('false'), new Boolean(false)])).toEqual('[3,"false",false]');
+
+        // String-keyed array elements are not enumerable and make no sense in JSON
+        const a = ['foo', 'bar'];
+        a['baz'] = 'quux';      // a: [ 0: 'foo', 1: 'bar', baz: 'quux' ]
+        expect(JSONStringify(a)).toEqual('["foo","bar"]');
+        // 
+
+        expect(JSONStringify({ x: [10, undefined, function() {}, Symbol('')] })).toEqual('{"x":[10,null,null,null]}');
+        // 
+
+        // Standard data structures
+        expect(JSONStringify([new Set([1]), new Map([[1, 2]]), new WeakSet([{a: 1}]), new WeakMap([[{a: 1}, 2]])])).toEqual('[{},{},{},{}]');
+
+        
+        // Symbols:
+        expect(JSONStringify({ x: undefined, y: Object, z: Symbol('') })).toEqual('{}');
+        // '{}'
+        expect(JSONStringify({ [Symbol('foo')]: 'foo' })).toEqual('{}');
+        // '{}'
+        expect(JSONStringify({ [Symbol.for('foo')]: 'foo' }, [Symbol.for('foo')])).toEqual('{}');
+        // '{}'
+        
+        // with replacer
+        // JSONStringify({ [Symbol.for('foo')]: 'foo' }, (k, v) => {
+        //     if (typeof k === 'symbol') {
+        //     return 'a symbol';
+        // }
+        // });
+        // undefined
+
+        // Non-enumerable properties:
+        expect(JSONStringify(Object.create(null, { x: { value: 'x', enumerable: false }, y: { value: 'y', enumerable: true } }))).toEqual('{"y":"y"}');
+        // '{"y":"y"}'
+
+        // BigInt values throw
+        // JSONStringify({x: 2n});
+        // TypeError: BigInt value can't be serialized in JSON
+    })
+
+    it('cyclic data throws error', () => {
+        /**
+        * Throws a TypeError ("cyclic object value") exception when a circular reference is found.
+        */
+        let a = {};
+        a.x = a;
+        expect(() => {
+            JSONStringify(a)
+        }).toThrowError(new Error('cyclic object value'));
+    });
+
  });
+
+describe('all json parse tests', () => { 
+    it('primitive tsts', () => {
+
+    });
+})
