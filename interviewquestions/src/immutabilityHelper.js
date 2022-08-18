ed9cd5f49489457d594a8b59b6b65b07a05eefa5
chetnashah <chetshah80@gmail.com>
2022-08-18 10:32:24 +0530

    kgkjh

--- interviewquestions/src/immutabilityHelper.js
+++ interviewquestions/src/immutabilityHelper.js
@@ -9,11 +9,13 @@
 /**
  * The $-prefixed keys are called commands. 
  * The data structure they are "mutating" is called the target.
+ * 
+ * parentRef is important for $set and other cases where complete replace is necessary
  * @param {*} target 
  * @param {*} spec 
  * @returns 
  */
-function update(target, spec) {
+function update(target, spec, parentRef, parentKey) {
     console.log(target);
     console.log(spec);
 
@@ -23,13 +25,58 @@ function update(target, spec) {
     console.log(command)
     if(!command) {
         // recurse
+        const allKeys = Object.keys(target);
+        console.log(allKeys);
+        for(let i=0;i<allKeys.length;i++) {// collect data from all keys
+            const key = allKeys[i];
+            console.log(key);
+            if(!spec[key]){
+                continue;
+            }
+            console.log(key);
+            const updated = update(target[key], spec[key], target, key);// recursively ask to update
+
+            console.log(updated);
+            if(updated !== target[key]) {// if children were updated, we must fork the tree
+                if(Array.isArray(currTarget)) {
+                    const ans = [...currTarget];// not sure of direct pattern?
+                    ans[key] = updated;
+                    currTarget = [...ans];
+                } else {
+                    currTarget = {...currTarget, ...{[key]: updated}};
+                }
+            } else {// no need to fork the tree if children were not updated
+                // return currTarget;
+            }
+        }
+        return currTarget;
     } else { // command present
         switch(command) {
-            case 'push':
-                currTarget = [...currTarget, ...(currSpec['$push'])];
-                break;
-            case 'set':
-                break;
+            case 'push': {
+                return [...currTarget, ...(currSpec['$push'])];
+            }
+            case 'set': {
+                console.log(currTarget);
+                console.log(currSpec);
+                console.log(parentRef);
+                console.log(parentKey)
+                const ans = {...parentRef, [parentKey]: currSpec['$set']};
+                console.log(ans);
+                return currSpec['$set'];
+            }
+            case 'merge': {
+                if(typeof currTarget != "object") {
+                    throw new Error('object expected');
+                }
+                console.log(currTarget);
+                console.log(currSpec);
+                console.log(parentRef);
+                console.log(parentKey);
+                const parentStuff = parentKey ? {...parentRef[parentKey]} : {...currTarget};// no parent means we are at top level so spread toplevel
+                const ans = {...parentStuff, ...(currSpec['$merge'])};
+                console.log(ans);
+                return ans;
+            }
         }
     }
 
@@ -46,6 +93,12 @@ function getCommand(obj) {
     }
     if(allKeys.includes('$set')) {
         return 'set';
+    }
+    if(allKeys.includes('$apply')){
+        return 'apply';
+    }
+    if(allKeys.includes('$merge')) {
+        return 'merge';
     }
 }
 
--- interviewquestions/src/immutabilityHelper.test.js
+++ interviewquestions/src/immutabilityHelper.test.js
@@ -17,47 +17,88 @@ describe('immutabilityHelper', () => {
         expect(newObj).toEqual({a: {b: [1,2,3,5,6]}});
     });
 
-    // it('update object with set', () => {
-    //     const state = {
-    //         a: {b: {c: 1}},
-    //         d: 2
-    //       };
+    it('bfe tes 1', () => {
+        expect(update([1], {$push: [2, 3]})).toEqual([1, 2, 3])
+    })
+
+    it('bfe test 2', () => {
+        expect(update({a: [1]}, {a: {$push: [2, 3]}})).toEqual({a: [1, 2, 3]})
+    });
+
+    it('bfe test 3', () => {
+        expect(() => update(1, {$push: [2, 3]}))
+        .toThrow()      
+    });
+
+    // it('bfe test 4', () => {
+    //     expect(update([1], {1: {$set: 2}})).toEqual([1, 2])
+    // });
+
+    it('bfe test 5', () => {
+        expect(update({a: {b: 1}}, {a: { b: {$set: 2}}})).toEqual({ a: { b: 2 } })
+    });
+
+    it('bfe test 6', () => {
+        expect(update({a: {b: 1}}, {a: {$merge: {c: 3}}})).toEqual({ a: { b: 1, c: 3 } })
+    });
+
+    it('bfe test 7', () => {
+        // merge on same existing key overrides value
+        expect(update({a: {c: 1}}, {a: {$merge: {c: 3}}})).toEqual({ a: { c: 3 } })
+    });
+
+    it('bfe test 8', () => {
+        // trying to merge non-object with object should throw
+        expect(() => update({a: 1}, {a: {$merge: {c: 3}}})).toThrow()
+    });
+
+    // it('bfe test 9', () => {
+    //     // pass the current value and apply the function
+    //     expect(update([1], {0: {$apply: (item) => item * 2}}))
+    //     .toEqual([2])      
+    // });
+    it('update object with set', () => {
+        const state = {
+            a: {b: {c: 1}},
+            d: 2
+          };
           
-    //       const newState = update(
-    //         state, 
-    //         {a: {b: { c: {$set: 3}}}}
-    //       );
+          const newState = update(
+            state, 
+            {a: {b: { c: {$set: 3}}}}
+          );
         
-    //       // check original has not changed
-    //       expect(state).toEqual({
-    //         a: {b: {c: 1}},
-    //         d: 2
-    //       });
+          // check original has not changed
+          expect(state).toEqual({
+            a: {b: {c: 1}},
+            d: 2
+          });
 
-    //       // check new is correct
-    //       expect(newState).toEqual({
-    //         a: {b: {c: 3}},
-    //         d: 2
-    //       });
-    // });
+          // check new is correct
+          expect(newState).toEqual({
+            a: {b: {c: 3}},
+            d: 2
+          });
+    });
 
-    // it('update array with set', () => {
-    //     const arr = [1, 2, 3, 4]
-    //     const newArr = update(
-    //       arr, 
-    //       {0: {$set: 0}}
-    //     );
+    it('update array with set', () => {
+        const arr = [1, 2, 3, 4]
+        debugger;
+        const newArr = update(
+          arr, 
+          {0: {$set: 0}}
+        );
 
-    //     expect(arr).toEqual([1,2,3,4]);
-    //     expect(newArr).toEqual([0,2,3,4]);
-    // });
+        expect(arr).toEqual([1,2,3,4]);
+        expect(newArr).toEqual([0,2,3,4]);
+    });
 
-    // it('update Shallow merge', () => {
-    //     const obj = {a: 5, b: 3};
-    //     const newObj = update(obj, {$merge: {b: 6, c: 7}}); // => {a: 5, b: 6, c: 7}
-    //     expect(obj).toEqual({a: 5, b: 3});
-    //     expect(newObj).toEqual({ a: 5, b: 6, c: 7});        
-    // });
+    it('update Shallow merge', () => {
+        const obj = {a: 5, b: 3};
+        const newObj = update(obj, {$merge: {b: 6, c: 7}}); // => {a: 5, b: 6, c: 7}
+        expect(obj).toEqual({a: 5, b: 3});
+        expect(newObj).toEqual({ a: 5, b: 6, c: 7});        
+    });
 
     // it('update apply', () => {
     //     const arr = [1, 2, 3, 4]
@@ -68,14 +109,14 @@ describe('immutabilityHelper', () => {
     //     expect(newArr).toEqual([2,2,3,4]);
     // });
 
-    // it('multiple operators, set and push', () => {
-    //     const myData = {x: { y: {z: null}}, a:{ b: []}};
-    //     const newData = update(myData, {
-    //         x: {y: {z: {$set: 7}}},
-    //         a: {b: {$push: [9]}}
-    //     });
+    it('multiple operators, set and push', () => {
+        const myData = {x: { y: {z: null}}, a:{ b: []}};
+        const newData = update(myData, {
+            x: {y: {z: {$set: 7}}},
+            a: {b: {$push: [9]}}
+        });
 
-    //     expect(myData).toEqual({x: { y: {z: null}}, a:{ b: []}});
-    //     expect(newData).toEqual({x: { y: {z: 7}}, a:{ b: [9]}});
-    // });
+        expect(myData).toEqual({x: { y: {z: null}}, a:{ b: []}});
+        expect(newData).toEqual({x: { y: {z: 7}}, a:{ b: [9]}});
+    });
  })
